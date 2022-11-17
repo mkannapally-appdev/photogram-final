b21db7bd3dc9eaa21e0d45e6398c9e06db59e945
mkannapally <akannapa@chicagobooth.edu>
2022-11-17 06:25:14 +0000

    follow status working on users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all.order({ :username => :asc })
 
+    @signed_user = User.all.where({:id => session.fetch(:user_id)}).first
 
     render({:template => "users/index.html.erb"})
   end
